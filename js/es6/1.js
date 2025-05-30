451cedd3d43ecb49fe7af0442ef773da867db857
LgFvm9353 <2790928695@qq.com>
2025-05-30 21:11:39 +0800

    es6新特性

---
+++ js/es6/1.js
@@ -0,0 +1,5 @@
+const name = '旺财';
+const age = 2;
+// 告别字符串拼接  更优雅的字符串模板
+const sentence = `${name} is ${age * 7} years old`; // 输出：旺财 is 2 years old
+console.log(sentence); // 输出：旺财 is 2 years old
