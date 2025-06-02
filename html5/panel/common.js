30f3337689dcadf808db487d8fc9f881ebec9f1a
LgFvm9353 <2790928695@qq.com>
2025-06-02 14:22:03 +0800

    将click事件变为mouseenter事件

--- html5/panel/common.js
+++ html5/panel/common.js
@@ -2,8 +2,7 @@ const panels = document.querySelectorAll('.qq-panel');
 panels.forEach(panel => {
     // JS 是事件机制的语言
     // 每一个元素上监听
-    panel.addEventListener('click', () => {
-        console.log('biubiubiu');
+    panel.addEventListener('mouseenter', () => {
         removeActiveClasses(); //模块化
         panel.classList.add("qq-panel_active");
     })
