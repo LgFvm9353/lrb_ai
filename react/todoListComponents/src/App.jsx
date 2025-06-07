9c63de18d722c36a2e1811e29adf47126e34741a
LgFvm9353 <2790928695@qq.com>
2025-06-07 14:19:18 +0800

    新增react组件化

---
+++ react/todoListComponents/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+import TodoList from './components/TodoList.jsx'
+function App() {
+
+  return (
+    <>
+      <div></div>
+      <TodoList/>
+      
+    </>
+  )
+}
+
+export default App
