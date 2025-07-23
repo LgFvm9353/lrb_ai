6725f5a01d5affb188388dc48c24f7850acc55ed
LgFvm9353 <2790928695@qq.com>
2025-07-23 11:09:36 +0800

    新增jwt token生成

--- react/api-demos/jwt-demo/README.md
+++ react/api-demos/jwt-demo/README.md
@@ -1,3 +1,29 @@
 # jwt 登录鉴权
 - isLogin user 全局状态 zustand 
 - mock 登录模拟
+   - apifox api请求模拟
+   不用写页面，就可以发送请求 
+   curl 
+
+- 会话授权
+  - 服务器知道我们是谁？
+  - http是无状态的
+     - 请求头 cookie 
+     - server 种下一个cookie 唯一 sid 值 
+     - 每次请求中从 cookie 读取 sid
+     - 服务器就根据 sid 知道我们是谁
+
+  - 登录和用户鉴权方案JWT
+    - {id: 112, username: 'good',level:4}
+    - 一种算法 生成一个hash 串
+    - token 服务器端令牌
+    - 带上token
+    - decode 解码
+
+  - jsonwebtoken
+    jwt 鉴权的库
+    sign 颁发一个token user
+    verify 验证token user
+    - pnpm i jwt
+    - import jwt from 'jsonwebtoken'
+    
--- react/api-demos/jwt-demo/mock/login.js
+++ react/api-demos/jwt-demo/mock/login.js
@@ -1,4 +1,9 @@
 // login 模块 mock
+import pkg from 'jsonwebtoken';
+const { sign } = pkg;
+// 安全性 编码的时候加密
+// 解码的时候用于解密
+const secret = '!&5d646hcode';
 export default [
   {
     url: '/api/login',
@@ -6,13 +11,34 @@ export default [
     timeout: 2000,
     response: (req,res) => {
       const { username, password } = req.body;
-      if(username === 'admin' && password === '123456') {
+      if(username !== 'admin' || password !== '123456') {
         return{
-          code: 0,
-          message: 'success',
-          token: 'admin-token'
+          code: 1,
+          message:'用户名或密码错误',
+          data: null
         }
-      } 
+      }
+      const token = sign(
+        {
+          user: {
+            id: "001",
+            username: "admin"
+          }
+        },
+        secret,          
+        { expiresIn: '1h' }  
+      );
+      return {
+        code: 0,
+        message: 'success',
+        data: {  // 添加data层保持一致性
+          token: token,
+          userInfo: {
+            id: "001",
+            username: "admin"
+          }
+        }
+      };
       
     }
   }
