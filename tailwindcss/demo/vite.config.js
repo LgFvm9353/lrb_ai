0a97f658ad0e7d7b73da04627a268e98c8ab4428
LgFvm9353 <2790928695@qq.com>
2025-08-09 14:50:08 +0800

    tailwindcss

---
+++ tailwindcss/demo/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer()
+      ]
+    }
+  }
+});
