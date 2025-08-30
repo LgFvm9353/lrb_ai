a2a2f2eb12dc8646ab5b3a9caba66387a3e65a75
LgFvm9353 <2790928695@qq.com>
2025-08-30 10:08:04 +0800

    闭包

---
+++ interview/js/closure/1.js
@@ -0,0 +1,11 @@
+function createCounter(){
+    let count = 0
+
+    return {
+        inc: ()=> ++count,
+        get: ()=> count
+    }
+}
+const counter = createCounter()
+console.log(counter.inc())
+console.log(counter.get())
